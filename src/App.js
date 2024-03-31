6656505e6e7ae088b45b6c501476a334752adbf7
shivakumar <shivakumar.yevunja@gmail.com>
2024-03-31 22:20:46 +0530

    added the code for rendering the name and phone dynamically

--- src/App.js
+++ src/App.js
@@ -4,15 +4,43 @@ import './App.css';
 
 
 class App extends Component{
+  
   render(){
+    let subscribers = [
+      {
+        id : 1,
+        name : "vishnu",
+        phone : '84567898765'
+      },
+      {
+        id : 2,
+        name : "hari",
+        phone : '84567898765' 
+      },
+      {
+        id : 3,
+        name : "shiva",
+        phone : '84567898765' 
+      }
+    ] 
     return(
+      
       <div>
       <Header />
       <button>Add</button>
       <div className='container'>
-        <span>Name</span><br/>
-        <span>Phone</span>
+        <span className='nameText'>Name</span><br/>
+        <span className='phoneText'>Phone</span>
       </div>
+
+      {
+        subscribers.map(sub =>{
+          return <div key={sub.id} className='container'>
+          <span >{sub.name}</span><br/>
+          <span >{sub.phone}</span>
+        </div>
+        })
+      }
     </div>
     )
   }
